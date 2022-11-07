020edfa0299de699cdc2fbc59c8d7bbc5e105a1c
Aleksandar Milakovic <a.milakovic@vega.local>
2022-11-07 13:02:15 +0100

    additional changes based on pr review

--- src/routes/todo.routes/createToDo.ts
+++ src/routes/todo.routes/createToDo.ts
@@ -1,24 +1,22 @@
 import { Router, Request, Response } from "express";
 import {
   validateBody,
-  createToDoObject,
+  createToDo,
 } from "../../services/todo.services/todoServices";
 import { saveTodo } from "../../repository/todo.repository/todoRepository";
 
-export function todoRouter(): Router {
+export function createToDoRouter(): Router {
   const todoRouter = Router();
 
-  return todoRouter.post("/", async (req: Request, res: Response) => {
+  return todoRouter.post("/", (req: Request, res: Response) => {
     const bodyValidationResult = validateBody(req.body);
     if (bodyValidationResult?.error) {
       return res.status(400).send({ error: bodyValidationResult.error });
     }
     const { title, description } = req.body;
-    const toDo = createToDoObject(title, description);
-    const saveOutcome = await saveTodo(toDo);
-    if (saveOutcome?.error) {
-      return res.status(400).send({ error: saveOutcome.error });
-    }
+    const toDo = createToDo(title, description);
+    saveTodo(toDo);
+
     return res.status(200).send({ message: "TO DO CREATED" });
   });
 }
--- src/routes/todo.routes/index.ts
+++ src/routes/todo.routes/index.ts
@@ -1,16 +1,16 @@
 import { Router } from "express";
-import { todoRouter as createToDoItem } from "./createToDo";
-import { todoRouter as deleteToDo } from "./deleteToDo";
-import { todoRouter as getToDoItem } from "./getToDo";
-import { todoRouter as getToDosItems } from "./getToDos";
+import { createToDoRouter } from "./createToDo";
+import { deleteToDoRouter } from "./deleteToDo";
+import { getToDoRouter } from "./getToDo";
+import { getToDosRouter } from "./getToDos";
 
 export default function toDoRoutes(): Router {
   const toDoRoutes = Router();
 
-  toDoRoutes.use("/todo", createToDoItem());
-  toDoRoutes.use("/todo", getToDosItems());
-  toDoRoutes.use("/todo", getToDoItem());
-  toDoRoutes.use("/todo", deleteToDo());
+  toDoRoutes.use("/todo", createToDoRouter());
+  toDoRoutes.use("/todo", getToDosRouter());
+  toDoRoutes.use("/todo", getToDoRouter());
+  toDoRoutes.use("/todo", deleteToDoRouter());
 
   return toDoRoutes;
 }
